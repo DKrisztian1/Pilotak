ebc610456b47d2b2d9b6dd0547f87986729b1a91
DKrisztian1 <dkrisztian2005@gmail.com>
2023-05-19 10:09:30 +0200

    asdasd

---
+++ ConsoleApp2/PilotaClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilotakClass
+{
+    internal class Pilota
+    {
+        string nev;
+        string szulEv;
+        string nemzetiseg;
+        int rajtszam;
+
+        public Pilota(string nev, string szulEv, string nemzetiseg, int rajtszam)
+        {
+            this.nev = nev;
+            this.szulEv = szulEv;
+            this.nemzetiseg = nemzetiseg;
+            this.rajtszam = rajtszam;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public string SzulEv { get => szulEv; set => szulEv = value; }
+        public string Nemzetiseg { get => nemzetiseg; set => nemzetiseg = value; }
+        public int Rajtszam { get => rajtszam; set => rajtszam = value; }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using PilotakClass;
+using System.IO;
+
+StreamReader sr = new StreamReader("pilotak.csv");
+List<Pilota> Pilotak = new List<Pilota>();
+sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    string[] adatok = sr.ReadLine().Split(";");
+    Pilota sor = new Pilota(adatok[0], adatok[1], adatok[2], Convert.ToInt32(adatok[3]));
+    Pilotak.Add(sor);
+}
